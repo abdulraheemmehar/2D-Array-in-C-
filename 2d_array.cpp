4924359ed883498985c36c9cd0398e152bef7865
Abdul Raheem <abdulraheemmeharzada@gmail.com>
2025-03-07 15:30:22 +0500

    Add project files.

---
+++ 2d_array.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	const int R = 3;
+	const int C = 3;
+
+	int **x = new int* [R];
+
+	for (int i = 0; i < R; i++)
+			x[i] = new int[C];
+
+	x[0][1] = 10;
+
+	cout << *(*(x + 0) + 1) << endl;
+
+	cout << x[0][1] << endl;
+
+	return 0;
+	/*cout << "Enter number of rows : ";
+	cin >> rows;
+	cout << "Enter number of columns : ";
+	cin >> col;
+	int** seating = new int* [rows];*/
+
+
+}
